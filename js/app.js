ddab4c1b6fc9d9835e09b13f44087baa61cffbea
kenan238 <73741117+kenan238@users.noreply.github.com>
2021-11-26 19:12:18 +0200

    Big Fix + Separate Interpreter and other shit

--- js/app.js
+++ js/app.js
@@ -1,78 +1,81 @@
+/* Interpreter */
 function interpreter(FRIEDCHICKEN, Friedchicken) {
-const chicken = (CHICKEN, Chicken) => {
-	Chicken &&( chicken. chicken =[,
-	CHICKEN, CHICKEN = Chicken = chicken.
-	$Chicken =-( CHICKEN ==( chicken.
-	Chicken = Chicken ))], chicken.
-	chicken [Chicken++] = chicken. chicken, chicken.
-	CHICKEN = ++Chicken, chicken (--Chicken), chicken.
-	$Chicken = ++Chicken, chicken. CHICKEN++ );
-	Chicken = chicken. Chicken [chicken.
-	$Chicken++ ]; chicken. Chicken = CHICKEN? Chicken?
-	'\012'== Chicken? chicken (++ CHICKEN, chicken.
-	chicken [++ chicken. CHICKEN ]=
-	CHICKEN - CHICKEN ): Chicken
-	==' '|'\015'== Chicken ||
-	(Chicken   )== "c" &  chicken. Chicken [chicken.
-	$Chicken++ ]== "h" &  chicken. Chicken [chicken.
-	$Chicken++ ]== "i" &  chicken. Chicken [chicken.
-	$Chicken++ ]== "c" &  chicken. Chicken [chicken.
-	$Chicken++ ]== "k" &  chicken. Chicken [chicken.
-	$Chicken++ ]== "e" &  chicken. Chicken [chicken.
-	$Chicken++ ]== "n"&&++chicken. chicken [chicken.
-	CHICKEN]? chicken (CHICKEN)
-	:[ "Error on line "+CHICKEN+": expected 'fried' or 'chicken'",
-		chicken. CHICKEN = CHICKEN ++- CHICKEN ]:
-	chicken. chicken :( CHICKEN = chicken.
-	Chicken[chicken.CHICKEN], Chicken? (Chicken =
+	const chicken = (CHICKEN, Chicken) => {
+		Chicken &&( chicken. chicken =[,
+		CHICKEN, CHICKEN = Chicken = chicken.
+		$Chicken =-( CHICKEN ==( chicken.
+		Chicken = Chicken ))], chicken.
+		chicken [Chicken++] = chicken. chicken, chicken.
+		CHICKEN = ++Chicken, chicken (--Chicken), chicken.
+		$Chicken = ++Chicken, chicken. CHICKEN++ );
+		Chicken = chicken. Chicken [chicken.
+		$Chicken++ ]; chicken. Chicken = CHICKEN? Chicken?
+		'\012'== Chicken? chicken (++ CHICKEN, chicken.
+		chicken [++ chicken. CHICKEN ]=
+		CHICKEN - CHICKEN ): Chicken
+		==' '|'\015'== Chicken ||
+		(Chicken   )== "c" &  chicken. Chicken [chicken.
+		$Chicken++ ]== "h" &  chicken. Chicken [chicken.
+		$Chicken++ ]== "i" &  chicken. Chicken [chicken.
+		$Chicken++ ]== "c" &  chicken. Chicken [chicken.
+		$Chicken++ ]== "k" &  chicken. Chicken [chicken.
+		$Chicken++ ]== "e" &  chicken. Chicken [chicken.
+		$Chicken++ ]== "n"&&++chicken. chicken [chicken.
+		CHICKEN]? chicken (CHICKEN)
+		:[ "Error on line "+CHICKEN+": expected 'fried' or 'chicken'",
+			chicken. CHICKEN = CHICKEN ++- CHICKEN ]:
+		chicken. chicken :( CHICKEN = chicken.
+		Chicken[chicken.CHICKEN], Chicken? (Chicken =
 
-	--Chicken? --Chicken? --Chicken? --Chicken? --Chicken?
-	--Chicken? --Chicken? --Chicken? --Chicken?
-	chicken. CHICKEN++ &&
-	--Chicken :'&#'+CHICKEN+';': chicken.
-	Chicken [chicken. Chicken [-- chicken. CHICKEN ]&&
-	(chicken. $Chicken += CHICKEN), --chicken.
-	CHICKEN ]: chicken. Chicken [chicken.
-	Chicken [CHICKEN] = chicken. Chicken
-	[-- chicken. CHICKEN ],-- chicken. CHICKEN ]:
-	chicken. Chicken [chicken. Chicken [chicken.
-	$Chicken++ ]] [CHICKEN]: CHICKEN == chicken.
-	Chicken [-- chicken. CHICKEN ]:
-	CHICKEN*chicken. Chicken [-- chicken.
-	CHICKEN ]: chicken. Chicken [-- chicken.
-	CHICKEN ]- CHICKEN: chicken. Chicken [-- chicken.
-	CHICKEN ]+ CHICKEN: chicken.
-	CHICKEN ++ && "chicken", chicken.
-	Chicken [chicken. CHICKEN ]= Chicken, chicken
-	()): CHICKEN );
+		--Chicken? --Chicken? --Chicken? --Chicken? --Chicken?
+		--Chicken? --Chicken? --Chicken? --Chicken?
+		chicken. CHICKEN++ &&
+		--Chicken :'&#'+CHICKEN+';': chicken.
+		Chicken [chicken. Chicken [-- chicken. CHICKEN ]&&
+		(chicken. $Chicken += CHICKEN), --chicken.
+		CHICKEN ]: chicken. Chicken [chicken.
+		Chicken [CHICKEN] = chicken. Chicken
+		[-- chicken. CHICKEN ],-- chicken. CHICKEN ]:
+		chicken. Chicken [chicken. Chicken [chicken.
+		$Chicken++ ]] [CHICKEN]: CHICKEN == chicken.
+		Chicken [-- chicken. CHICKEN ]:
+		CHICKEN*chicken. Chicken [-- chicken.
+		CHICKEN ]: chicken. Chicken [-- chicken.
+		CHICKEN ]- CHICKEN: chicken. Chicken [-- chicken.
+		CHICKEN ]+ CHICKEN: chicken.
+		CHICKEN ++ && "chicken", chicken.
+		Chicken [chicken. CHICKEN ]= Chicken, chicken
+		()): CHICKEN );
 
-	return chicken.
-	Chicken
-}
-const occurences = (string, subString, allowOverlapping) => {
-
-	string += "";
-	subString += "";
-	if (subString.length <= 0) return (string.length + 1);
-
-	var n = 0,
-		pos = 0,
-		step = allowOverlapping ? 1 : subString.length;
-
-	while (true) {
-		pos = string.indexOf(subString, pos);
-		if (pos >= 0) {
-			++n;
-			pos += step;
-		} else break;
+		return chicken.
+		Chicken
 	}
-	return n;
-}
-if(occurences(Friedchicken, "fried", false) < 3)
-	return "Error: put more 'fried' in your code."
+	const occurences = (string, subString, allowOverlapping) => {
+
+		string += "";
+		subString += "";
+		if (subString.length <= 0) return (string.length + 1);
+
+		var n = 0,
+			pos = 0,
+			step = allowOverlapping ? 1 : subString.length;
+
+		while (true) {
+			pos = string.indexOf(subString, pos);
+			if (pos >= 0) {
+				++n;
+				pos += step;
+			} else break;
+		}
+		return n;
+	}
+	if(occurences(Friedchicken, "fried", false) < 3)
+		return "Error: put more 'fried' in your code."
+	}
 	Friedchicken=Friedchicken.replace(/fried/g, "chicken")
 	return chicken(FRIEDCHICKEN, Friedchicken)
 }
+/* Interpreter */
 function friedChicken(inp, code) {
 	try {
 		var v=interpreter(inp, code);
